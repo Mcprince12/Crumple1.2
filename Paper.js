cdbf3d968deae5e2c3802278d3a939146f10d781
Mcprince12 <68846631+Mcprince12@users.noreply.github.com>
2020-09-07 09:04:42 +0200

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -6,8 +6,8 @@ class Paper{
             friction: 0.5,
             density: 1.2
         }
-        this.body = Bodies.circle(100, 50, 70, options);
-        this.radius = 50;
+        this.body = Bodies.circle(100, 50, 52.5, options);
+        this.radius = 25;
         this.image = loadImage("imgs/ooga.png");
         World.add(world, this.body);
     }
--- sketch.js
+++ sketch.js
@@ -53,7 +53,7 @@ function draw() {
 
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(paper.body, paper.body.position, {x:400, y:-350})
+		Matter.Body.applyForce(paper.body, paper.body.position, {x:400, y:-400})
 	}
 }
 
